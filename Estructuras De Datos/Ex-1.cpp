b35396147bd3d0b0e229caca2a53669fb54a2fdc
Sebastian <sebasorjuela@gmail.com>
2021-08-09 15:05:57 -0500

    Rectificacion Listas

--- Estructuras De Datos/Ex-1.cpp
+++ Estructuras De Datos/Ex-1.cpp
@@ -8,83 +8,17 @@ struct nodo
     nodo *siguiente;
 };
 
-struct lista
+void insertarLista(nodo *&Lista, int n)
 {
-    nodo *cabeza;
-    nodo *cola;
-    nodo *espacio;
-    lista()
-    {
-        cabeza = NULL;
-
-        cola = NULL;
-    }
-    void Adicionar(int valor)
-    {
-        nodo *temp = new nodo;
-        temp->dato = valor;
-        temp->siguiente = NULL;
-
-        if (cabeza == NULL)
-        {
-            cabeza = temp;
-            cola = temp;
-        }
-        else
-        {
-            cola->siguiente = temp;
-            cola = cola->siguiente;
-        }
-    }
-
-    void Mostrar()
-    {
-        nodo *temp = new nodo;
-        temp = cabeza;
-        while (temp != NULL)
-        {
-            cout << temp->dato << endl;
-            temp = temp->siguiente;
-        }
-    }
-
-    void BorrarPrimerdato()
-    {
-        if(cabeza ==  NULL)
-        {
-            
-            cout <<"La lista esta vacia";
-            return;
-        }
-    }
-    void Insertarenlista(int Dato, int pos)
-    {
-        nodo *temp = new nodo;
-        temp->dato = Dato;
-        for (int i = 0; i < 10; i++)
-        {
-            /* code */
-        }
-        
-        cout <<cabeza<<endl;
-        cout <<cola<<endl;
-    }
-};
+    nodo *nuevo_nodo =  new nodo();
+    nuevo_nodo->dato = n;
+    nodo *aux1 = Lista;
+    nodo *aux2;
+}
 
 
 int main(int argc, char const *argv[])
 {
-    lista primera;
-    int ops,valins;
-    primera.Adicionar(100);
-    primera.Adicionar(299);
-    cout << "Tenemos una lista enlazada "<<endl;
-    cout<<"digite el valor a insertar : ";
-    cin >>valins;
-    cout <<"digite el espacio donde desea ingresar el dato\n";
-    cout <<"1. cabeza\n";
-    cout <<"2. cola\n";
-    cin >> ops;
-    primera.Insertarenlista(valins,ops);
+    nodo *lista = NULL;
     return 0;
 }

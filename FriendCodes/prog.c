1ae240415c9fc22e1efb63ec801280619387adba
Sebastian <sebasorjuela@gmail.com>
2021-08-09 23:42:44 -0500

    Codigo C para el pana carlos

---
+++ FriendCodes/prog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char palabra[20];
+    int n;
+    scanf("%s",palabra);
+    scanf("%d",&n);
+    system("clear");
+    for (int i = 0; i < ("%d",n) ; i++)
+    {
+        printf("%s\n", palabra);
+    }
+    printf("\n");
+    return 0;
+}

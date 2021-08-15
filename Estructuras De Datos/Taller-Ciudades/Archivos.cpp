32cfeda0918f2315202ffda05799d613951bd332
Sebastian <sebasorjuela@gmail.com>
2021-08-15 02:44:12 -0500

    version final 15 - 08

--- Estructuras De Datos/Taller-Ciudades/Archivos.cpp
+++ Estructuras De Datos/Taller-Ciudades/Archivos.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <stdlib.h>
 
 using namespace std;
 
@@ -15,7 +16,7 @@ void mostrarlista(Nodo *lista)
     actual = lista;
     while (actual != NULL)
     {
-        cout << actual->dato<<" | \n";
+        cout << actual->dato << " | \n";
         actual = actual->siguiente;
     }
 }
@@ -23,23 +24,6 @@ void InsertarLista(Nodo *&lista, string n)
 {
     Nodo *nuevo_nodo = new Nodo();
     nuevo_nodo->dato = n;
-    Nodo *aux1 = lista;
-    Nodo *aux2;
-
-    while ((aux1 != NULL) && (aux1->dato < n))
-    {
-        aux2 =  aux1;
-        aux1 = aux1->siguiente;
-    }
-    if (lista == aux1)
-    {
-        lista = nuevo_nodo;
-    }
-    else
-    {
-        aux2->siguiente = nuevo_nodo;
-    }
-    nuevo_nodo->siguiente = aux1;
     mostrarlista(lista);
 }
 void menu()
@@ -50,40 +34,46 @@ void menu()
     string linea;
     do
     {
-        cout <<"Menu\n";
-        cout <<"1. leer archivo y cargar la lista\n";
-        cout <<"2. Mostrar las ciudades en la lista\n";
-        cout <<"3. salir";
-    } while (opcion != 3);
-
-    switch (opcion)
-    {
-    case 1:
-        
-        if (!entrada)
+        cout << "Menu\n";
+        cout << "1. leer archivo y cargar la lista\n";
+        cout << "2. Mostrar las ciudades en la lista\n";
+        cout << "3. salir\n";
+        cout << "opcion :";
+        cin >> opcion;
+        switch (opcion)
         {
-            cout <<"no se pudo abrir el archivo de texto";
-        }
-        else
-        {
-            while (entrada)
-            {
-                while(getline(entrada,linea)){
-                    InsertarLista(lista,linea);
+            case 1:
+                if (!entrada)
+                {
+                    cout << "no se pudo abrir el archivo de texto";
                 }
-                cout <<"Elementos " <<"insertados en la lista correctamente\n";
-            }
+                else
+                {
+                    while (entrada)
+                    {
+                        while (getline(entrada, linea))
+                        {
+                            InsertarLista(lista, linea);
+                        }
+                        cout << "Elementos " << "insertados en la lista correctamente\n";
+                        
+                    }
+                }
+                entrada.close();
+                break;
+            case 2:
+                mostrarlista(lista);
+                cout <<"\n";
+                
+                break;
+            default:
+                break;
         }
-        entrada.close();
-        break;
-    
-    default:
-        break;
-    }
-    
+
+    } while (opcion != 3);
 }
 int main(int argc, char const *argv[])
 {
-    
+    menu();
     return 0;
 }

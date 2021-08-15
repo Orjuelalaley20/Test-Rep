705e8fe9cf78775f86ddd3c552659df2467fec2e
Sebastian <sebasorjuela@gmail.com>
2021-08-14 23:06:45 -0500

    avance 14

--- Estructuras De Datos/Taller-Ciudades/Archivos.cpp
+++ Estructuras De Datos/Taller-Ciudades/Archivos.cpp
@@ -4,16 +4,46 @@
 
 using namespace std;
 
-struct nodo
+struct Nodo
 {
-    int dato;
-    nodo *siguiente;
+    string dato;
+    Nodo *siguiente;
 };
+void mostrarlista(Nodo *lista)
+{
+    Nodo *actual = new Nodo();
+
+}
+void InsertarLista(Nodo *&lista, string n)
+{
+    Nodo *nuevo_nodo = new Nodo();
+    nuevo_nodo->dato = n;
+    Nodo *aux1 = lista;
+    Nodo *aux2;
+
+    while ((aux1 != NULL) && (aux1->dato > n))
+    {
+        aux2 =  aux1;
+        aux1 = aux1->siguiente;
+    }
+    if (lista == aux1)
+    {
+        lista = nuevo_nodo;
+    }
+    else
+    {
+        aux2->siguiente = nuevo_nodo;
+    }
+    nuevo_nodo->siguiente = aux1;
+    
+    
+}
 
 int main(int argc, char const *argv[])
 {
     ifstream entrada("ciudades.txt");
-    char *linea =  new char[255];
+    string linea;
+    Nodo *lista = NULL;
     if (!entrada)
     {
         cout <<"no se pudo abrir el archivo de texto";
@@ -22,11 +52,12 @@ int main(int argc, char const *argv[])
     {
         while (entrada)
         {
-            entrada.getline(linea,300,'\n');
-            cout << linea<<"\n";
+            while(getline(entrada,linea)){
+                InsertarLista(lista,linea);
+            }
+            cout <<"Elementos " <<"insertados en la lista correctamente\n";
         }
     }
     entrada.close();
     return 0;
 }
-
